f9967eead58bc52d6aa6a891add1a987f5bf6358
tbodv1 <tboydv1@gmail.com>
2020-03-27 18:22:21 +0100

    project setup

---
+++ src/main/java/com/insurance/ussdservice/UssdserviceApplication.java
@@ -0,0 +1,13 @@
+package com.insurance.ussdservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UssdserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UssdserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insurance/ussdservice/UssdserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurance.ussdservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UssdserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
