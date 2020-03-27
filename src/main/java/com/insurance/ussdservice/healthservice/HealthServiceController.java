c574bb077a1b27b1c06b6533a0fe26da58df1733
tbodv1 <tboydv1@gmail.com>
2020-03-27 19:47:42 +0100

    system properties

---
+++ src/main/java/com/insurance/ussdservice/healthservice/HealthServiceController.java
@@ -0,0 +1,23 @@
+package com.insurance.ussdservice.healthservice;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthServiceController {
+
+    private String sessionId;
+
+    @PostMapping("https://ussdhealthapp.herokuapp.com/")
+    public String ussdService(@RequestParam String sessionId, @RequestParam String text) {
+
+        if (text == "1") {
+
+            return "END Choose account information you want to view";
+        }
+        return "CON Choose account information you want to view";
+    }
+}
